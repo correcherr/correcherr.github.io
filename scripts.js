b7e92ee7768d8664b63abfa6fb7c9a31e1254ed6
correcherr <lopezoscarcorrecher@gmail.com>
2025-01-29 14:43:23 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -44,3 +44,10 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+ // Script para manejar el menú desplegable
+ const menuToggle = document.querySelector('.menu-toggle');
+ const navUl = document.querySelector('.menu');
+
+ menuToggle.addEventListener('click', () => {
+     navUl.classList.toggle('show'); // Alterna la clase que muestra el menú
+ });
