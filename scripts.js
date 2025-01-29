89595b2915e4ac0ef9c102166d7c58a1b44d51c9
correcherr <lopezoscarcorrecher@gmail.com>
2025-01-29 22:03:04 +0100

    redemption

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
+// Script para manejar el menú desplegable
+const menuToggle = document.querySelector('.menu-toggle');
+const navUl = document.querySelector('.menu');
 
- // Script para manejar el menú desplegable
- const menuToggle = document.querySelector('.menu-toggle');
- const navUl = document.querySelector('.menu');
+menuToggle.addEventListener('click', () => {
+    navUl.classList.toggle('show'); // Alterna la clase que muestra el menú
+});
 
- menuToggle.addEventListener('click', () => {
-     navUl.classList.toggle('show'); // Alterna la clase que muestra el menú
- });
+document.addEventListener('DOMContentLoaded', (event) => {
+    const yearSpan = document.getElementById('current-year');
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+});
